1c33bf4c63132cd0b8ceeb354db9db0ffe91cc1b
abrahamgcc <abrahamgcc99@gmail.com>
2021-10-07 08:47:00 +0200

    build_kernel: Export CROSS_COMPILE

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,9 @@
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
+export CROSS_COMPILE=../tc/bin/aarch64-linux-android-
 
+make clean
 make ARCH=arm64 exynos9830-r8slte_defconfig
 make ARCH=arm64 -j64
 
